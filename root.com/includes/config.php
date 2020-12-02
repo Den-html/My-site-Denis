b8f38ed5b9f819db50fc221ace1b313fa7ad34cb
Den-html <71197583+Den-html@users.noreply.github.com>
2020-12-02 13:19:10 +0700

    Add files via upload

---
+++ root.com/includes/config.php
@@ -0,0 +1,11 @@
+<?php
+
+$config = array(
+	'title' => 'Блог',
+	'vk_url' => 'https://vk.com/id613687397',
+	'db' => array(
+		'server' => 'localhost',
+		'username' => 'root',
+		'password' => 'root',
+		'name' => 'blog')
+);
---
+++ root.com/index.php
@@ -0,0 +1,96 @@
+<?php
+
+$connection = mysqli_connect('127.0.0.1', 'root', 'root','blog');
+?>
+<!DOCTYPE html>
+<html lang='ru'>
+
+<head>
+    <meta charset='utf-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
+    <title><?php echo $config['title'];?></title>
+	<link rel="shortcut icon" href="\media\favicon.ico" type="image/x-icon">
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@1,200&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Nerko+One&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300&display=swap" rel="stylesheet">
+<style>
+a.main {
+  text-decoration: none;
+  outline: none;
+  display: inline-block;
+  margin: 10px 20px;
+  padding: 15px 30px;
+  overflow: hidden;
+  border: 2px solid;
+  border-bottom-width: 4px;
+  font-family: 'Montserrat', sans-serif;
+  text-transform: uppercase;
+  font-weight: bold;
+  letter-spacing: 2px;
+  color: rgba(30,255,188,1);
+  background: rgba(255,255,255,1);
+  transition: color .3s, background .5s;
+}
+a.main:hover {
+  animation: stripes .75s infinite linear;
+  background: linear-gradient(45deg, rgba(30,255,188,1) 25%, rgba(255,255,255,1) 25%, rgba(255,255,255,1) 50%, rgba(30,255,188,1) 50%,      rgba(30,255,188,1) 75%, rgba(255,255,255,1) 75%, rgba(255,255,255,1));
+  background-size: 10px 10px;
+  color: #FF50E5;
+}
+@keyframes stripes {
+  0% {background-position: 0 0;}
+  100% {background-position: 50px 0;}
+}
+
+</style>
+</head>
+
+<body>
+
+	<div id="wrapper">
+
+		<header id="header">
+			<div class="header_top">
+				<div class="container">
+					<div class="header_top_logo">
+						<h1 style="font-family: Raleway"><?php echo $config['title']; ?></h1>
+						<h1 style="font-family: Nerko One"><?php
+echo date("D, F j, Y, g:i a");
+						?></h1>
+					</div>
+					<nav class="header_top_menu">
+						<ul>
+							<a href="/" style="font-family: Roboto Slab" class="main">Главная</a>
+							<a href="/pages/about_me.php" style="font-family: Roboto Slab " class="main">Обо мне</a>
+							<a href="https://vk.com/id613687397" target="_blank" style="font-family: Roboto Slab" class="main">Я Вконтакте</a>
+							<a href="../pages/articles.php" style="font-family: Roboto Slab " class="main"> Статьи</a>
+						</ul>
+					</nav>
+				</div>
+			</div>
+
+			<div class="header_bottom">
+				<div class="container">
+					<nav>
+					</nav>
+				</div>
+			</div>
+		</header>
+<?php
+echo "<hr>";
+$articles = mysqli_query($connection, "SELECT * FROM `articles`");
+while ($art = mysqli_fetch_assoc($articles) ) {
+	?>
+	<?php
+}
+?>
+<img src="/media/new.gif">
+
+</body>
+
+</html>
